9e3713331c30e0b87bd7cdd93e0b5540d6e3c665
Xelias12 <alexis.reigne@live.fr>
2023-06-15 22:18:53 +0200

    Début du tuto

---
+++ demo.php
@@ -0,0 +1,13 @@
+salut
+<?php
+// différents type de variable nombre, chaine de caractère booleen,null
+// le ' n'interpréte jamais contrairement au " qui permet aussi de faire des caractère spéciaux tel que le retour à la ligne
+
+$Prenom = 'Alexis';
+$Nom='Reigne';
+
+echo $Prenom."/n ".$Nom;
+
+?>
+
+yoyoyphp
